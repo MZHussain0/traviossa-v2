8415cd339104fb93916fe08a716ecf69757a991c
mzhussain0 <mzhussain092@gmail.com>
2023-04-04 05:20:40 +0530

    implement listing creation - image upload support

---
+++ app/components/inputs/ImageUpload.tsx
@@ -0,0 +1,55 @@
+﻿"use client";
+
+import { FC, useCallback } from "react";
+import { CldUploadWidget } from "next-cloudinary";
+import Image from "next/image";
+import { TbPhotoPlus } from "react-icons/tb";
+
+interface ImageUploadProps {
+  onChange: (value: string) => void;
+  value: string;
+}
+
+declare global {
+  var cloudinary: any;
+}
+
+const ImageUpload: FC<ImageUploadProps> = ({ onChange, value }) => {
+  const handleUpload = useCallback(
+    (result: any) => {
+      onChange(result.info.secure_url);
+    },
+    [onChange]
+  );
+
+  return (
+    <CldUploadWidget
+      onUpload={handleUpload}
+      uploadPreset="bc49jqyl"
+      options={{ maxFiles: 1 }}>
+      {({ open }) => {
+        return (
+          <div
+            onClick={() => open?.()}
+            className="relative cursor-pointer hover:opacity-70 transition border-2 border-dashed p-20 border-violet-400 flex flex-col items-center justify-center gap-4 text-neutral-300">
+            <TbPhotoPlus size={50} />
+            <div className="font-semibold text-lg">Click to upload</div>
+
+            {value && (
+              <div className="absolute inset-0 w-full h-full">
+                <Image
+                  alt="upload"
+                  fill
+                  style={{ objectFit: "cover" }}
+                  src={value}
+                />
+              </div>
+            )}
+          </div>
+        );
+      }}
+    </CldUploadWidget>
+  );
+};
+
+export default ImageUpload;
