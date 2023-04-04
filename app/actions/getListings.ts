7ffc2f35812fc607409058f36d6945c7e39fad9d
mzhussain0 <mzhussain092@gmail.com>
2023-04-04 22:50:37 +0530

    implement listing fetch functionality

---
+++ app/actions/getListings.ts
@@ -0,0 +1,14 @@
+﻿import prismaClient from "../libs/prismaDb";
+
+export default async function getListings() {
+  try {
+    const listings = await prismaClient.listing.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+    return listings;
+  } catch (error: any) {
+    throw new Error(error);
+  }
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,34 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+import getCurrentUser from "./actions/getCurrentUser";
+import getListings from "./actions/getListings";
+import ClientOnly from "./components/ClientOnly";
+import Container from "./components/Container";
+import EmptyState from "./components/EmptyState";
+import ListingCard from "./components/listings/ListingCard";
 
-const inter = Inter({ subsets: ["latin"] });
+export default async function Home() {
+  const listings = await getListings();
+  const currentUser = await getCurrentUser();
 
-export default function Home() {
-  return <div className=""></div>;
+  if (listings.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState showReset />
+      </ClientOnly>
+    );
+  }
+  return (
+    <ClientOnly>
+      <Container>
+        <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+          {listings.map((listing) => (
+            <ListingCard
+              currentUser={currentUser}
+              key={listing.id}
+              data={listing}
+            />
+          ))}
+        </div>
+      </Container>
+    </ClientOnly>
+  );
 }
