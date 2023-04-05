8f397406fd78c823e9856b82fe6d9e99370cc7ea
mzhussain0 <mzhussain092@gmail.com>
2023-04-05 23:13:43 +0530

    implement listing reservation & calendar component

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -5,14 +5,14 @@ import { MapContainer, Marker, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
 import markerIcon from "leaflet/dist/images/marker-icon.png";
-// import markerShadow from "leaflet/dist/images/marker-shadow.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
 
 //@ts-ignore
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
   iconUrl: markerIcon.src,
   iconRetinaUrl: markerIcon2x.src,
-  // ShadowUrl: markerShadow.src,
+  ShadowUrl: markerShadow.src,
 });
 interface MapProps {
   center?: number[];
---
+++ app/components/listings/ListingReservation.tsx
@@ -0,0 +1,50 @@
+﻿"use client";
+import { FC } from "react";
+import { Range } from "react-date-range";
+import Calendar from "../inputs/Calendar";
+import Button from "../Button";
+
+interface ListingReservationProps {
+  price: number;
+  totalPrice: number;
+  dateRange: Range;
+  onChangeDate: (value: Range) => void;
+  onSubmit: () => void;
+  disabled?: boolean;
+  disabledDates: Date[];
+}
+
+const ListingReservation: FC<ListingReservationProps> = ({
+  price,
+  totalPrice,
+  dateRange,
+  onChangeDate,
+  onSubmit,
+  disabledDates,
+  disabled,
+}) => {
+  return (
+    <div className="bg-slate-900 rounded-xl overflow-hidden border border-neutral-400">
+      <div className="flex flex-row items-center gap-1 p-4">
+        <div className="text-2xl font-semibold">$ {price}</div>
+        <div className=" font-light text-neutral-400"> night</div>
+      </div>
+      <hr />
+      <Calendar
+        value={dateRange}
+        disabledDates={disabledDates}
+        onChange={(value) => onChangeDate(value.selection)}
+      />
+      <hr />
+      <div className="p-4">
+        <Button disabled={disabled} label="Reserve" onClicK={onSubmit} />
+      </div>
+      <div className="p-4 flex flex-row items-center justify-between font-semibold text-lg">
+        <div className="">Total</div>
+        <div className="">${totalPrice}</div>
+      </div>
+    </div>
+  );
+};
+
+export default ListingReservation;
