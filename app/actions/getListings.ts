c8e59838c7b1bffc3ca15928cc26e61e8edb0763
mzhussain0 <mzhussain092@gmail.com>
2023-04-06 22:55:41 +0530

    func: filtering search

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -2,15 +2,71 @@
 
 export interface IListingsParams {
   userId?: string;
+  guestCount?: number;
+  roomCount?: number;
+  bathroomCount?: number;
+  startDate?: string;
+  endDate?: string;
+  locationValue?: string;
+  category?: string;
 }
 
 export default async function getListings(params: IListingsParams) {
   try {
-    const { userId } = params;
+    const {
+      userId,
+      roomCount,
+      guestCount,
+      bathroomCount,
+      startDate,
+      endDate,
+      locationValue,
+      category,
+    } = params;
 
     let query: any = {};
     if (userId) {
       query.userId = userId;
+    }
+    if (category) {
+      query.category = category;
+    }
+    if (locationValue) {
+      query.locationValue = locationValue;
+    }
+    if (roomCount) {
+      query.roomCount = {
+        gte: +roomCount,
+      };
+    }
+    if (guestCount) {
+      query.guestCount = {
+        gte: +guestCount,
+      };
+    }
+    if (bathroomCount) {
+      query.bathroomCount = {
+        gte: +bathroomCount,
+      };
+    }
+
+    if (startDate && endDate) {
+      query.NOT = {
+        reservations: {
+          some: {
+            OR: [
+              {
+                endDate: { gte: startDate },
+                startDate: { lte: startDate },
+              },
+              {
+                startDate: { lte: endDate },
+                endDate: { gte: endDate },
+              },
+            ],
+          },
+        },
+      };
     }
 
     const listings = await prismaClient.listing.findMany({
--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -1,12 +1,16 @@
 ﻿"use client";
+import useSearchModal from "@/app/hooks/useSearchModal";
 import React from "react";
 import { BiSearch } from "react-icons/bi";
 
 type Props = {};
 
 const Search = (props: Props) => {
+  const searchModal = useSearchModal();
   return (
-    <div className="border w-full md:w-auto py-2 rounded-full shadow-sm hover:shadow-md transition cursor-pointer">
+    <div
+      onClick={searchModal.onOpen}
+      className="border w-full md:w-auto py-2 rounded-full shadow-sm hover:shadow-md transition cursor-pointer">
       <div className="flex flex-row items-center justify-between">
         <div className="text-xs font-semibold px-6">Anywhere</div>
         <div className="hidden sm:block text-xs font-semibold px-6 flex-1 text-center border-x">
